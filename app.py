de8c78614aa1dc6b06cf9fc60e638578960202f3
Greg <gkmarsella@gmail.com>
2018-01-11 18:39:16 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_modus import Modus
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import sys
 import os
